28c8df65e12db956fb602f635c86f5de61bd6d63
Juan Cantor <juan.cantor@utadeo.edu.co>
2017-11-08 21:51:31 -0500

    Uso de rest para valores de bitcoin

---
+++ MA_currency_bitcoin_converter/Controller/CurrencyBitcoinConverterModel.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyBitcoinConverter.swift
+//  MA_currency_bitcoin_converter
+//
+//  Created by Juan Cantor on 11/8/17.
+//  Copyright © 2017 UTadeo. All rights reserved.
+//
+
+import Foundation
+
+struct CurrencyBitcoinConverterModel{
+    let currencyArray = ["AUD","BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB"]
+}
--- MA_currency_bitcoin_converter/ViewController.swift
+++ MA_currency_bitcoin_converter/ViewController.swift
@@ -11,11 +11,15 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-
+    
     @IBOutlet weak var labelPrice: UILabel!
     @IBOutlet weak var currencyPickerView: UIPickerView!
     
     let currencyArray = ["AUD","BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB"]
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    
+    var finalURL : String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +27,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         currencyPickerView.delegate = self
         currencyPickerView.dataSource = self
     }
-
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -37,7 +41,28 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print(currencyArray[row])
+        finalURL = baseURL + currencyArray[row]
+        getBitCoinData(url: finalURL!)
+    }
+    
+    func getBitCoinData(url:String){
+        Alamofire.request(url,method:.get).responseJSON { (response) in
+            if response.result.isSuccess{
+                let bitcoinJSON : JSON = JSON(response.result.value!)
+                self.updateBitcoinData(json:bitcoinJSON)
+            } else {
+                print("Error: \(String(describing: response.result.error))")
+                self.labelPrice.text = "Se presentó un problema en la conexión"
+            }
+        }
+    }
+    
+    func updateBitcoinData(json:JSON){
+        if let bitcoinResult = json["ask"].double{
+            labelPrice.text = String(bitcoinResult)
+        } else {
+            labelPrice.text = "Servicio no disponible"
+        }
     }
 }
 
